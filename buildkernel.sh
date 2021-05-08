8e143d14031dffc02e8f869504b5f040093db6f1
Aniruddh Rao <anrao3@buffalo.edu>
2021-05-07 23:07:05 -0400

    Added bash script for quick(er) execution

---
+++ buildkernel.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+ROOTDIR="$HOME/os161/root" # Add your root dir here (where your sys.conf is)
+KERNDIR="$HOME/os161/kern" # Add location of your kern directiory in ospath 
+VERSION="compile/DUMBVM"   # Variable for future when this might change
+DEBUG=false		   # For gdb, set to true if want to make this default
+while [[ -n "$1" ]]; do	   # Just option stuff, thanks stackoverflow :)
+	case "$1" in
+	-d) DEBUG=true;;
+	-r) 
+		ROOTDIR="$2"
+		shift
+		;;
+	-o) 
+		KERNDIR="$2"
+		shift
+		;;
+	-v) 
+		VERSION="$2"
+		shift
+		;;
+	esac
+	shift
+done
+
+# Just the usual workflow from ASST0
+cd $KERNDIR/$VERSION
+bmake
+bmake install
+cd $ROOTDIR
+if $DEBUG; then
+	sys161 -w kernel
+else
+	sys161 kernel
+fi
+# alias <new name>='/home/<full path to script>/filename.sh' 
+# add to ~/.bashrc for making a command. Thanks stackoverflow :)
